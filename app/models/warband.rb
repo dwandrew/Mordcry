d165687c13342f88534bdf3cb4d68a62ac38649c
dwandrew <danwandrew@gmail.com>
2020-08-11 17:42:41 +0100

    adding css

--- app/models/warband.rb
+++ app/models/warband.rb
@@ -1,8 +1,7 @@
 class Warband < ApplicationRecord
     belongs_to :user
     has_many :warriors
-    has_many :warbands_equipment
-    has_many :equipment, through: :warbands_equipment
+    has_many :equipment, through: :warriors
 
     accepts_nested_attributes_for :warriors
 end
