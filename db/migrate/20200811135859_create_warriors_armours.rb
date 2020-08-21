0884c8f340b5e112d6ada16ab66646edce48c7aa
dwandrew <danwandrew@gmail.com>
2020-08-21 09:10:16 +0100

    Fixed error in warriors_armour join table with id as string instead of int

--- db/migrate/20200811135859_create_warriors_armours.rb
+++ db/migrate/20200811135859_create_warriors_armours.rb
@@ -1,8 +1,8 @@
 class CreateWarriorsArmours < ActiveRecord::Migration[6.0]
   def change
     create_table :warriors_armours do |t|
-      t.string :warrior_id
-      t.string :armour_id
+      t.integer :warrior_id
+      t.integer :armour_id
 
       t.timestamps
     end
